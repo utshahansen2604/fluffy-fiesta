38fd34a98e62e914b102d85b349f1811ab6ddb72
eswaran.karuppiah <eswaran.karuppiah@eswaranpiahsmbp.clients.asia-pac.shell.com>
2019-11-12 13:13:20 +0530

    updated variables

--- variables.tf
+++ variables.tf
@@ -6,5 +6,6 @@ variable "people" {
   type = map
   // contains a list of peoples names to create aws accounts for
   default = {
-    "eswarkp" : "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCxyuux2lSDTPZ/VO/g6SxTji3WdO88iiVMYsVe1rF9fVOrrm9Kcg9lRoFs28NC4nd+kRUUjB8GvYGfoUa8DVtcCj3X7SsjwG7ycHVz/UVIj1b5/eqdR7lZgZ4gbTkWejQszcAJ7pV6UigJ9mzCmCSO64E3W5XnyIDZ9zxJyKyIzkUej6i9z121ZWdP0tpOM177yCita8A5TJwPZMvjtycJmD27InoLYK1Kr+Z+v8zpmoVOC1qGpA6xJpOKGkDMPfisbAx1QJnxuyBImJqOP2HrMcePQM9TGHJtdxhfHTJXzZ57/iPtXwxky18HV1xZCbu3C4FelWd050/a0R/UnOx eswaran.karuppiah@eswaranpiahsmbp.clients.asia-pac.shell.com",}
+    "eswarkp" : "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCxyuux2lSDTPZ/VO/g6SxTji3WdO88iiVMYsVe1rF9fVOrrm9Kcg9lRoFs28NC4nd+kRUUjB8GvYGfoUa8DVtcCj3X7SsjwG7ycHVz/UVIj1b5/eqdR7lZgZ4gbTkWejQszcAJ7pV6UigJ9mzCmCSO64E3W5XnyIDZ9zxJyKyIzkUej6i9z121ZWdP0tpOM177yCita8A5TJwPZMvjtycJmD27InoLYK1Kr+Z+v8zpmoVOC1qGpA6xJpOKGkDMPfisbAx1QJnxuyBImJqOP2HrMcePQM9TGHJtdxhfHTJXzZ57/iPtXwxky18HV1xZCbu3C4FelWd050/a0R/UnOx eswaran.karuppiah@eswaranpiahsmbp.clients.asia-pac.shell.com",
+  }
 }
