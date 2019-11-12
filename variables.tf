3081b9f5aa37f640844232a610f784e339da0170
abhishek.sharma <abhishek.sharma@abhisheksharma-ka-MacBook-Pro.local>
2019-11-12 13:13:22 +0530

    Committed

--- variables.tf
+++ variables.tf
@@ -6,5 +6,5 @@ variable "people" {
   type = map
   // contains a list of peoples names to create aws accounts for
   default = {
-    "yogendraKS" : "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuZTwYLTb28FU585mqh9+kJKkPcYzADkevODsSizuyWst968YyzOQATWbQhNwFs44jvTfWG6SRxOzgphEa+XO9YmX2m5eZsk+rQIgNhJzpVjPM9CXqsRW+7amHvFHUWNLeeDNLuVUA2sItxjxrRpNrqSk+0CG/aO7levM1oxjkkh8w5vgvw4CvV62i72pYmsR9YgMbADWjgSHsT6xXMdYyEtwx/Xst1wi9nLJiGx+dfkG+wjdHh278+OLrQnZUQy/5wYp932bObpgbkiBS7FOz+nDDW50oFoUDbN3uv45+2GpjObFdISg6hvDiqZ/JjUZ+ImMYWwrNbIXwc7B3VdbR yogendra.sharma@YogendraSharmas-MacBook-Pro.local",  }
+    "Abhishek5340":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmApkYq2ixYuvAQoZ77Gw+TILtsd4MA8CfYzGzKua+8MhGujY7Ui6GMDOL3Kd2tmMcI5vpxfK77XuLtloXJsaIgRcfVxq3LgSRqTENvkc77J/c8VCU8tw6jwHyq/SZ7OZbCDfdASpCxLVO9hEPGZBVHanTZP9wyKQ53LR+IRFijwp9hA64PrRj+zBIFyOaQ+8BoxpgYuaH0r+i+Og6AqnYMDB4PepBo/0EbK4gMnGz90Kix6MB0bIcrkMd18Kq+oXroiGrEpcFwbOVf0/FZNSd+XBrdexkY4Y9XxoeX3e+stVaoR4sX/7WUR+ej74qUWzthZolhRZTsHvwURxgUGdb abhishek.sharma@abhishermakambp.clients.asia-pac.shell.com ",  }
 9}
