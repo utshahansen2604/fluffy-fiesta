f93e847e31012614d9fe98f6c52e423657fcfe70
Abhishek.Arun <abhishek.arun@abhishearunsmbp.clients.asia-pac.shell.com>
2019-11-12 13:17:28 +0530

    added my SSHkey3

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "keybase" {
 }
 variable "people" {
   type = map
-  // contains a list of peoples names to create aws accounts for
+  //this  contains a list of peoples names to create aws accounts for
   default = {
     "upesabhi" : "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzkGI1+F+YpjfWQ5Wnummlnda1CcO2k/eHPOveRsp7D/kATv0g+qG/6CpEauic23s9xz+ci09AuUvo1T7LvCqzigGnOt7bsbdGBuxM7+0uO3y10MJxcui/Sjp8iu0lef+CMfJEjGXBkH/ELjIW99wzFxpW+GFwhu64kjuvOekyUuS2KE8a6V5v7D1Vmy0krLzVrcpOhUgq2zjvK9ZmgPHJZVT+A9IH0QIEUYHs2XeaOpg/BYyK8o6ws/t97k++g3FIitJqvuAamyBCs4xf44koRSFhXcr18QQOi80DFSGCV3PeSiWba7Fgduk18kRDnuk/lrZr49k7eODKmjawDMYz abhishek.arun@abhishearunsmbp.clients.asia-pac.shell.com",
   }
