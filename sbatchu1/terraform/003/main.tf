ac6e243197cf03914ee6210c073e459f715b9436
sairamgopal.batchu <sairamgopal.batchu@sairamgopalbatchus-MacBook-Pro.local>
2019-11-26 10:50:20 +0530

    tf changes

--- sbatchu1/terraform/003/main.tf
+++ sbatchu1/terraform/003/main.tf
@@ -1,24 +1,24 @@
 provider "aws" {
-	region = "ap-south-1"
-	profile = "ShellPowerUser"
+  region  = "ap-south-1"
+  profile = "ShellPowerUser"
 }
 
 variable "prefix" {
-	type = "string"
-	default = "terraform-sbatchu1"
-	description = "variable usage demonstration"
+  type        = "string"
+  default     = "terraform-sbatchu1"
+  description = "variable usage demonstration"
 }
 
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "terraform" {
-	bucket =  "${var.prefix}-${replace(lower(data.aws_caller_identity.current.user_id), ":", "-")}"
-	acl = "private"
-	tags = {
-		Environment = "sdu-boot-camp"
-		Owner = "sbatchu1"
-		Purpose = "to store my state files"
-	}
+  bucket = "${var.prefix}-${replace(lower(data.aws_caller_identity.current.user_id), ":", "-")}"
+  acl    = "private"
+  tags = {
+    Environment = "sdu-boot-camp"
+    Owner       = "sbatchu1"
+    Purpose     = "to store my state files"
+  }
 }
 
 output "s3_bucket_arn" {
--- sbatchu1/terraform/003/remote-state.tf
+++ sbatchu1/terraform/003/remote-state.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-sbatchu1-s3-backend1"
-        encrypt = "true"
-        key = "week3"
-        region = "ap-south-1"
-        profile = "ShellPowerUser"
-    }
+  backend "s3" {
+    bucket  = "terraform-sbatchu1-s3-backend1"
+    encrypt = "true"
+    key     = "week3"
+    region  = "ap-south-1"
+    profile = "ShellPowerUser"
+  }
 }
 
