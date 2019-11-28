0203a4172f7cecab71cd992ab70f891026363b33
sairamgopal.batchu <sairamgopal.batchu@sairamgopalbatchus-MacBook-Pro.local>
2019-11-28 20:35:07 +0530

    final changes

--- sbatchu1/docker/jenkins-eco/run-connect.sh
+++ sbatchu1/docker/jenkins-eco/run-connect.sh
@@ -2,8 +2,16 @@
 
 while ! curl --silent jenkins-master:8080 | grep 'You are authenticated'; do echo .; sleep 1; done
 
+while ! wget --quiet http://jenkins-master:8080/jnlpJars/jenkins-cli.jar -O /dev/null; do
+    sleep 1
+    echo 'agent - waiting for jenkins-cli'
+done
+
+#wait for a file to exist
+while [[ ! -f /root/jen-vol/initialAdminPassword ]]; do echo .; sleep 1; done
+
 password=$(cat /root/jen-vol/initialAdminPassword)
 
-sh cat $password
+echo $password
 
-sh connect-node.sh jenkins-master:8080 jenkins-agent admin:$password
+bash /connect-node.sh http://jenkins-master:8080 jenkins-agent admin:$password
--- sbatchu1/docker/jenkins-eco/run-create.sh
+++ sbatchu1/docker/jenkins-eco/run-create.sh
@@ -2,6 +2,15 @@
 
 while ! curl --silent jenkins-master:8080 | grep 'You are authenticated'; do echo .; sleep 1; done
 
+while ! wget --quiet http://jenkins-master:8080/jnlpJars/jenkins-cli.jar -O /dev/null; do
+    sleep 1
+    echo 'waiting for jenkins-cli'
+done
+
+while [[ ! -f /root/jen-vol/initialAdminPassword ]]; do echo .; sleep 1; done
+
 password=$(cat /root/jen-vol/initialAdminPassword)
 
-sh create-node.sh jenkins-master:8080 jenkins-agent admin:$password
+echo $password
+
+bash /create-node.sh http://jenkins-master:8080 jenkins-agent admin:$password
